8deaf791079de76212de29d69f6b9df9b01abd28
Hossain <arafat.kd99@gmail.com>
2023-07-19 16:14:31 +0100

    Update link with html to css and Fix webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
