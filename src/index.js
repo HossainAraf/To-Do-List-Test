424c095b8a823d8f45428ae3db66bbe4c2115c4a
Hossain <arafat.kd99@gmail.com>
2023-07-19 16:58:17 +0100

    Create object

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import _ from 'lodash';
 import './index.css';
 
 const renderList = document.querySelector('#render-list');
+
+const list = [
+  {
+    index: 0,
+    description: 'desc-1',
+    completed: true,
+  },
+  {
+    index: 1,
+    description: 'desc-2',
+    completed: true,
+  },
+]
+
